0d806821fcf28f52177337ae883b818098e8c163
Franco Reyes <fr.reyesh@duocuc.cl>
2025-09-30 14:56:23 -0300

    ViewModel terminado

--- app/src/main/java/com/example/viewmodela/MainActivity.kt
+++ app/src/main/java/com/example/viewmodela/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.viewmodela.navigation.AppNavigation
 import com.example.viewmodela.ui.theme.ViewModelATheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ViewModelATheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold { innerPadding ->
+                    //Aqui va el flujo de navegación
+                    Box(modifier = Modifier.padding(innerPadding))
+                    AppNavigation()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ViewModelATheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/viewmodela/navigation/AppNavigation.kt
@@ -0,0 +1,30 @@
+package com.example.viewmodela.navigation
+
+import ResumenScreen
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.viewmodela.ui.screen.RegistroScreen
+import com.example.viewmodela.viewmodel.UsuarioViewModel
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    //Aquí creamos el ViewModel una vez
+    val usuarioViewModel: UsuarioViewModel = viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = "registro"
+    ){
+        composable("registro"){
+            RegistroScreen(navController, usuarioViewModel)
+        }
+        composable("resumen"){
+            ResumenScreen(usuarioViewModel)
+        }
+    }
+}
