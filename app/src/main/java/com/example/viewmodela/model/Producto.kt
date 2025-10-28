ba8fe377c9ff48c0b0402ad20b89dc63f168c08f
Franco Reyes <fr.reyesh@duocuc.cl>
2025-10-28 15:51:51 -0300

    Producto model

---
+++ app/src/main/java/com/example/viewmodela/model/Producto.kt
@@ -0,0 +1,10 @@
+package com.example.viewmodela.model
+
+// atributo de Producto
+data class Producto(
+val nombre: String = "",
+val precio: String = "",
+val descripcion: String = "",
+val categoria: String = "",
+val imagen: String = ""
+)
